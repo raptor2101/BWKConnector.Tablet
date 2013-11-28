c58ec5897ba8ab936361743b97d7c41bd8653fce
raptor2101 <raptor2101@gmx.de>
2013-11-28 23:47:12 +0100

    CHG: Apply new LayoutInheritance

--- src/de/raptor2101/BattleWorldsKronos/Connector/Gui/Activities/GameListingActivity.java
+++ src/de/raptor2101/BattleWorldsKronos/Connector/Gui/Activities/GameListingActivity.java
@@ -1,41 +1,5 @@
 package de.raptor2101.BattleWorldsKronos.Connector.Gui.Activities;
 
-import java.util.List;
-
-import android.content.Intent;
-import android.widget.ListAdapter;
-import de.raptor2101.BattleWorldsKronos.Connector.Data.Entities.Game;
-import de.raptor2101.BattleWorldsKronos.Connector.Gui.GameViewAdapater;
-import de.raptor2101.BattleWorldsKronos.Connector.Gui.R;
-
-public class GameListingActivity extends AbstractGameListingActivity{
-  private GameViewAdapater mAdapter = new GameViewAdapater(this);
-  
-  public GameListingActivity() {
-    super(R.layout.game_listing_activity);
-  }
-
-  
-
-  @Override
-  protected void startSettingsActivity() {
-    Intent intent = new Intent(this, SettingsActivity.class);
-    startActivity(intent);
-  }
-
-
-
-  @Override
-  protected ListAdapter getGamesAdapter() {
-    return mAdapter;
-  }
-
-
-
-  @Override
-  protected void setGames(List<Game> games) {
-    mAdapter.setGames(games);
-    
-  }
+public class GameListingActivity extends ActionBarGameListingActivity{
   
 }
